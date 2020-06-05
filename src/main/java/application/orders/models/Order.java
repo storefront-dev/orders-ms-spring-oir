f0880543a3d4fd244200f366d7be2c01c93bb0b3
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-06-05 14:55:22 -0500

    forked orders-ms-spring from storefront

---
+++ src/main/java/application/orders/models/Order.java
@@ -0,0 +1,89 @@
+package application.orders.models;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "orders")
+/*
+ * define O-R mapping of order table
+ */
+public class Order {
+
+	@Id //primary key
+	@Column(name = "orderId")
+	@GeneratedValue(generator="uuid-generator")
+	@GenericGenerator(name="uuid-generator", strategy="uuid")
+	String id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "date")
+	Date date;
+	
+	@Basic
+	@Column(name = "itemId")
+	int itemId;
+
+	@Basic
+	@Column(name = "customerId")
+	String customerId;
+	
+	@Basic
+	@Column(name = "count")
+	int count;
+	
+	public Order() {
+		super();
+//		this.id = UUID.randomUUID().toString();
+		// TODO Auto-generated constructor stub
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customer_id) {
+		this.customerId = customer_id;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	@Override
+	public String toString() {
+		return "{id = " + id + ", itemId=" + itemId + ", date=" + date.toString() + ", customerId=" + customerId
+				+ ", count=" + count + "}";
+	}
+
+
+	
+}
